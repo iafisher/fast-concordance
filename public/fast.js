2f4c9f1a52c31d239eca2fc5c679a527921ea416
Ian Fisher <ian@iafisher.com>
2025-03-30 09:46:09 -0400

    show search stats

--- public/fast.js
+++ public/fast.js
@@ -1,11 +1,5 @@
-// const elementButton = document.getElementById("search-btn");
-// const elementInput = document.getElementById("search");
-// const elementResults = document.getElementById("results");
-
-
-async function search(keyword, resultsOut) {
+async function search(keyword, resultsOut, statsOut) {
     const startTime = performance.now();
-    let firstResultTime = null;
     // TODO: safe URL construction
     const httpResult = await fetch(`/concord?w=${keyword}`);
     const reader = httpResult.body.getReader();
@@ -15,8 +9,13 @@ async function search(keyword, resultsOut) {
     while (true) {
         const { done, value } = await reader.read();
         if (done) {
-            // TODO: does this drop the last result?
-            console.log("time to last result: %f ms", (performance.now() - startTime).toFixed(1));
+            statsOut.millisToLastResult = performance.now() - startTime;
+            if (statsOut.millisToFirstResult === null) {
+                // in case we got 0 results
+                statsOut.millisToFirstResult = statsOut.millisToLastResult;
+            }
+            // TODO: does this drop the last result? b/c buffer might not be empty
+            m.redraw();
             break;
         }
         buffer += decoder.decode(value, { stream: true });
@@ -35,9 +34,8 @@ async function search(keyword, resultsOut) {
                 resultsOut.push(data);
                 m.redraw();
 
-                if (firstResultTime === null) {
-                    firstResultTime = performance.now();
-                    console.log("time to first result: %f ms", (firstResultTime - startTime).toFixed(1))
+                if (statsOut.millisToFirstResult === null) {
+                    statsOut.millisToFirstResult = performance.now() - startTime;
                 }
             }
         }
@@ -48,17 +46,22 @@ class PageView {
     constructor() {
         this.keyword = "";
         this.results = [];
+        this.stats = { millisToFirstResult: null, millisToLastResult: null };
     }
 
     view() {
         return m("main", [
             m(InputView, { onEnter: (keyword) => this.onEnter(keyword) }),
+            m(StatsView, { stats: this.stats, resultsCount: this.results.length }),
             m(ResultsView, { keyword: this.keyword, results: this.results })]);
     }
 
     onEnter(keyword) {
         this.keyword = keyword;
-        search(this.keyword, this.results);
+        this.results = [];
+        this.stats.millisToFirstResult = null;
+        this.stats.millisToLastResult = null;
+        search(this.keyword, this.results, this.stats);
     }
 }
 
@@ -81,11 +84,32 @@ class InputView {
     }
 }
 
+class StatsView {
+    view(vnode) {
+        // TODO: more stats: books searched, books matched, bytes searched?
+        const stats = vnode.attrs.stats;
+        const resultsCount = vnode.attrs.resultsCount;
+        if (stats.millisToFirstResult === null) {
+            return null;
+        }
+
+        const s = resultsCount === 1 ? "" : "s";
+        const firstMs = stats.millisToFirstResult.toFixed(1);
+        let doneAfter = "";
+        if (stats.millisToLastResult !== null && stats.millisToLastResult !== stats.millisToFirstResult) {
+            const lastMs = stats.millisToLastResult.toFixed(1);
+            doneAfter = `(done after ${lastMs}ms)`;
+        }
+        return m("div.stats",
+            `${resultsCount} result${s} in ${firstMs}ms ${doneAfter}`);
+    }
+}
+
 class ResultsView {
     view(vnode) {
         const results = vnode.attrs.results;
         const keyword = vnode.attrs.keyword;
-        return m("div.results", 
+        return m("div.results",
             [
                 m("div.side.left", results.map(result => m("div", result.left))),
                 m("div.center", results.map(_ => m("div", keyword))),
