fdfbf8a3f01d45245786a36860672ef162c2eff3
Ian Fisher <ian@iafisher.com>
2025-04-18 00:59:50 +0000

    fix bug with simd impl

--- internal/simdsearch/simdsearch.go
+++ internal/simdsearch/simdsearch.go
@@ -8,6 +8,18 @@ package simdsearch
 // #include <stdio.h>
 // #include <string.h>
 //
+// void print_avx_vector(const char* label, __m256i x) {
+// 	int x1 = _mm256_extract_epi32(x, 0);
+// 	int x2 = _mm256_extract_epi32(x, 1);
+// 	int x3 = _mm256_extract_epi32(x, 2);
+// 	int x4 = _mm256_extract_epi32(x, 3);
+// 	int x5 = _mm256_extract_epi32(x, 4);
+// 	int x6 = _mm256_extract_epi32(x, 5);
+// 	int x7 = _mm256_extract_epi32(x, 6);
+// 	int x8 = _mm256_extract_epi32(x, 7);
+// 	printf("%s: %d %d %d %d %d %d %d %d\n", label, x1, x2, x3, x4, x5, x6, x7, x8);
+// }
+//
 // ssize_t simd_str_search(_GoString_ text, _GoString_ keyword, size_t offset) {
 //	size_t text_len = _GoStringLen(text);
 //	size_t keyword_len = _GoStringLen(keyword);
@@ -19,16 +31,22 @@ package simdsearch
 //	const char* keyword_p = _GoStringPtr(keyword);
 // 	for (size_t i = 0; i < keyword_len; i++) {
 // 		keyword_padded[i] = keyword_p[i];
-// 		keyword_mask[i] = 1;
+// 		keyword_mask[i] = 0xFF;
 // 	}
 // 	__m256i keywordv = _mm256_loadu_si256((__m256i*)keyword_padded);
 // 	__m256i mask = _mm256_loadu_si256((__m256i*)keyword_mask);
+//	// print_avx_vector("keywordv", keywordv);
+//	// print_avx_vector("mask", mask);
 //
 //	const char* text_p = _GoStringPtr(text);
 // 	for (size_t i = offset; i <= text_len - 32; i++) {
+// 		// printf("index: %lu\n", i);
 // 		__m256i textv = _mm256_loadu_si256((__m256i*)(text_p + i));
+//		// print_avx_vector("textv", textv);
 // 		__m256i r1 = _mm256_xor_si256(textv, keywordv);
+//		// print_avx_vector("r1", r1);
 // 		__m256i r2 = _mm256_and_si256(r1, mask);
+//		// print_avx_vector("r2", r2);
 // 		if (_mm256_testz_si256(r2, r2)) {
 // 			return i;
 // 		}
--- internal/simdsearch/simdsearch_test.go
+++ internal/simdsearch/simdsearch_test.go
@@ -20,8 +20,25 @@ func TestSearch(t *testing.T) {
 		t.Fatal()
 	}
 
-	i = Search("asdfhjkasdfhklasdhfklasdjfhjkasdjhfjkasdhvajkdv", "lasdh", 14)
+	expectNoMatch(t, "asdfhjkasdfhklasdhfklasdjfhjkasdjhfjkasdhvajkdv", "lasdh", 14)
+	expectNoMatch(t, " Gerani Gerani Gerani Gerani Gerani Gerani", "vampire", 0)
+	expectNoMatch(t, "And in the window pots of perfumed flowers, Geraniums, asters, wallflowers, violets. And in one window stood the traveller.", "vampire", 0)
+}
+
+func expectMatch(t *testing.T, pos int, text string, keyword string, offset int) {
+	i := Search(text, keyword, offset)
+	if i == -1 {
+		t.Errorf("expected to find '%s' in '%s' but didn't", keyword, text)
+	} else if i != pos {
+		t.Errorf("expected to find '%s' in '%s' at %d but found instead at %d", keyword, text, pos, i)
+	}
+}
+
+func expectNoMatch(t *testing.T, text string, keyword string, offset int) {
+	t.Helper()
+
+	i := Search(text, keyword, offset)
 	if i != -1 {
-		t.Fatal()
+		t.Errorf("expected no match but got one at i=%d: '%s'", i, text[i:i+len(keyword)])
 	}
 }
